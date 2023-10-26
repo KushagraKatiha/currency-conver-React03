41273f215f9f7847c7a88fa4495462c8ede5c6b2
kushagrakatiha <kushagrakatiha123@gmail.com>
2023-10-26 16:10:07 +0530

    design completed

--- src/App.jsx
+++ src/App.jsx
@@ -15,16 +15,22 @@ function App() {
   const optionList = Object.keys(currencyInfo)
 
   return (
-    <InputBox
-              label="To"
-              amount={amount}
-              currencyOptions={optionList}
-              // onCurrencyChange={(currency)=>{
-              //   setTo(currency)
-              // }}
-              // selectCurrency={from}
-              // amountDisable
-            />
+    <div className='w-full h-screen flex flex-wrap justify-center bg-cover bg-no-repeat'
+    style={{
+      backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`}}
+    >
+      <div className='w-1/2 h-auto m-auto mt-auto p-9 backdrop-blur-lg rounded-2xl'>
+        <div className='mb-1'>
+          <InputBox/>
+        </div>
+        <div className='w-full h-0.5'>
+          <button className='absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5'>Swap</button>
+        </div>
+        <div className='mt-1'>
+          <InputBox/>
+        </div>
+      </div>
+    </div>
   )
 }
 
